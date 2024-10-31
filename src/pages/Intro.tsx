9e997cfd97f451fb254dda17fd4d492c1f19d6d0
KumJungMin <rmawjdals@naver.com>
2024-11-01 00:10:46 +0900

    feat: add intro warning message

--- src/pages/Intro.tsx
+++ src/pages/Intro.tsx
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 import Background_img from '../assets/Background_Img.png';
 import ButtonGrid from '../components/ButtonGrid';
 import FooterButton from '../components/common/FooterButton';
+import Icon from '../components/common/Icon';
 import { LABEL_MAPPING } from '../consts/label';
 
 const Intro: React.FC = () => {
@@ -37,6 +38,14 @@ const Intro: React.FC = () => {
         clickedLabels={clickedLabels}
         setClickedLabels={setClickedLabels}
       />
+      <WarningText>
+        <Icon name="alert-triangle" width={16} height={16} />
+        <Text>
+          본 서비스에서 제공되는 위험도 계산과 정보는 참고용으로 제공되며 어떠한
+          법적 효력도 없습니다. 이를 바탕으로 발생하는 문제에 대해 책임을 지지
+          않습니다.
+        </Text>
+      </WarningText>
       <FooterButton
         onClick={handleNextPage}
         disabled={
@@ -77,6 +86,27 @@ const Title = styled.div`
 const Description = styled.div`
   ${({ theme }) => theme.typography.Body};
   color: ${({ theme }) => theme.colors.gray500};
+`;
+
+const WarningText = styled.div`
+  position: absolute;
+  bottom: 76px;
+  ${({ theme }) => theme.typography.Body};
+  color: ${({ theme }) => theme.colors.red500};
+  display: flex;
+  width: 327px;
+  padding: 12px 16px;
+  align-items: center;
+  gap: 10px;
+  border-radius: 9px;
+  background-color: rgba(255, 255, 255, 0.88);
+  font-size: 12px;
+  font-weight: 400;
+  letter-spacing: -0.12px;
+`;
+
+const Text = styled.div`
+  flex: 1;
 `;
 
 export default Intro;
