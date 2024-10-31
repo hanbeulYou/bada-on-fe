c05d97f1b26da1f05d6b98610c7744fd95dffd71
hanbeulYou <hanbeul.you@gmail.com>
2024-10-31 09:25:09 +0900

    feat: Router 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
