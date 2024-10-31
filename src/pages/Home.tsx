25658c4a7d66ad868074a79012f7cef32cb6666a
hanbeulYou <hanbeul.you@gmail.com>
2024-10-31 16:23:12 +0900

    feat: BottomSheet 컴포넌트 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+import BottomSheet from '../components/BottomSheet';
+import RollList from '../components/RollList';
+import Search from '../components/Search';
+import SearchBar from '../components/SearchBar';
 
 function Home() {
-  return <div>Home</div>;
+  const [isSearchPage, setIsSearchPage] = useState(false);
+  const [searchValue, setSearchValue] = useState('');
+
+  return (
+    <Container>
+      <Header>
+        {isSearchPage ? (
+          <CloseButton onClick={() => setIsSearchPage(false)}>X</CloseButton>
+        ) : null}
+        <SearchBar
+          onClick={() => !isSearchPage && setIsSearchPage(prev => !prev)}
+          searchValue={searchValue}
+          setSearchValue={setSearchValue}
+        />
+      </Header>
+      {isSearchPage ? (
+        <Search searchValue={searchValue} />
+      ) : (
+        <>
+          <RollList />
+          <MapTmp />
+          <BottomSheet>
+            <div>Hello World</div>
+          </BottomSheet>
+        </>
+      )}
+    </Container>
+  );
 }
+
+const MapTmp = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: aqua;
+`;
+
+const Container = styled.div``;
+
+const Header = styled.header`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const CloseButton = styled.div`
+  display: flex;
+  padding-left: 16px;
+`;
 
 export default Home;
