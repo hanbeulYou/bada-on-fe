6fc0f61a232c52bd48f580d8eeba8f1adfbc2e0f
hanbeulYou <hanbeul.you@gmail.com>
2024-12-02 01:57:28 +0900

    feat: safeArea 저장 로직 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
+import { useEffect } from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 import { AddressProvider } from './context/AddressProvider';
+import { SafeAreaProvider } from './context/SafeAreaProvider';
 import Home from './pages/Home';
 import Intro from './pages/Intro';
 import NotFound from './pages/NotFound';
 import { Layout } from './styles/Layout';
 
 function App() {
+  useEffect(() => {
+    const safeAreaInsets = (window as any).safeAreaInsets;
+    if (safeAreaInsets) {
+      dispatch({
+        type: 'SET_SAFE_AREA',
+        payload: safeAreaInsets,
+      });
+    }
+  }, []);
   return (
-    <AddressProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Intro />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </Router>
-    </AddressProvider>
+    <SafeAreaProvider>
+      <AddressProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Intro />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="*" element={<NotFound />} />
+            </Route>
+          </Routes>
+        </Router>
+      </AddressProvider>
+    </SafeAreaProvider>
   );
 }
 
