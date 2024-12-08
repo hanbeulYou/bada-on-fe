082eb836ec7531aa19a9372d5b95caa4074cad27
hanbeulYou <hanbeul.you@gmail.com>
2024-12-09 07:52:38 +0900

    fix: 액티비티 아이콘 및 라벨 수정

--- src/consts/label.ts
+++ src/consts/label.ts
@@ -1,34 +1,44 @@
-export type Label = '스노클링' | '다이빙' | '해수욕' | '서핑' | '카약/패들보드';
+export type Label =
+  | '스노클링'
+  | '다이빙'
+  | '해수욕'
+  | '카약/패들보드'
+  | '서핑'
+  | '스쿠버다이빙';
 
 export type Activity =
   | 'snorkeling'
   | 'diving'
   | 'swimming'
+  | 'kayakingPaddleBoarding'
   | 'surfing'
-  | 'kayakingPaddleBoarding';
+  | 'scuba';
 
 export const LABELS: Label[] = [
   '스노클링',
   '다이빙',
   '해수욕',
-  '서핑',
   '카약/패들보드',
+  '서핑',
+  '스쿠버다이빙',
 ];
 
-export const DISABLED_LABELS: Label[] = ['해수욕', '서핑', '카약/패들보드'];
+export const DISABLED_LABELS: Label[] = ['스쿠버다이빙'];
 
 export const LABEL_MAPPING: Record<Label, Activity> = {
   스노클링: 'snorkeling',
   다이빙: 'diving',
   해수욕: 'swimming',
-  서핑: 'surfing',
   '카약/패들보드': 'kayakingPaddleBoarding',
+  서핑: 'surfing',
+  스쿠버다이빙: 'scuba',
 };
 
 export const LABEL_MAPPING_REVERSE: Record<Activity, Label> = {
   snorkeling: '스노클링',
   diving: '다이빙',
   swimming: '해수욕',
-  surfing: '서핑',
   kayakingPaddleBoarding: '카약/패들보드',
+  surfing: '서핑',
+  scuba: '스쿠버다이빙',
 };
