c7d951284db06ca2dc70581c002f83788e382124
hanbeulYou <hanbeul.you@gmail.com>
2024-12-09 07:16:42 +0900

    feat: sentry saas 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import * as Sentry from '@sentry/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
@@ -6,6 +7,11 @@ import { ThemeProvider } from 'styled-components';
 import App from './App.tsx';
 import GlobalStyle from './styles/GlobalStyle.ts';
 import theme from './styles/theme.ts';
+
+Sentry.init({
+  dsn: 'https://f956924d307031f7f16ab6f58dbe8538@o4508425511567360.ingest.us.sentry.io/4508425513074688',
+  integrations: [],
+});
 
 const queryClient = new QueryClient({
   defaultOptions: {
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "hanbeul-you",
+    project: "javascript-react"
+  })],
+
   server: {
     host: '0.0.0.0',
   },
+
+  build: {
+    sourcemap: true
+  }
 });
