89e6ec95be7f1770f5256904700c7f5ed5e5ed20
hanbeulYou <hanbeul.you@gmail.com>
2025-01-04 20:15:33 +0900

    feat: Info에 들어가는 정보 라벨 추가

---
+++ src/components/info/SummaryContent.tsx
@@ -0,0 +1,50 @@
+import { styled } from 'styled-components';
+
+interface SummaryContentProps {
+  contentType: 'weather' | 'wind' | 'temperature' | 'wave' | 'tide';
+  content: string;
+}
+
+const GetEmoji = ({ contentType, content }: SummaryContentProps) => {
+  if (contentType === 'weather') {
+    if (content === '맑음') return '☀️';
+    if (content === '흐림') return '🌥';
+    if (content === '비') return '🌧';
+    if (content === '천둥/번개') return '⛈';
+    if (content === '눈') return '❄️';
+  } else if (contentType === 'wind') {
+    if (content === '매우 약함') return '🍃️️';
+    if (content === '약함') return '🍃️️';
+    if (content === '적당') return '🍂';
+    if (content === '강함') return '🌪';
+    if (content === '매우 강함') return '🌪';
+  } else {
+    return '🌡️️';
+  }
+};
+
+const SummaryContent = ({ contentType, content }: SummaryContentProps) => {
+  return (
+    <SummaryContentContainer>
+      <GetEmoji contentType={contentType} content={content} />
+      <SummaryContentText>{content}</SummaryContentText>
+    </SummaryContentContainer>
+  );
+};
+
+const SummaryContentContainer = styled.div`
+  display: flex;
+  padding: 12px;
+  gap: 4px;
+  align-items: center;
+  border-radius: 4px;
+  background-color: ${({ theme }) => theme.colors.gray50};
+  flex: 1;
+`;
+
+const SummaryContentText = styled.span`
+  color: ${({ theme }) => theme.colors.gray600};
+  ${({ theme }) => theme.typography.Body};
+`;
+
+export default SummaryContent;
